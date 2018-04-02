4df30286f954c44547625cbd01372596ee50c30a
w9d <w9d@localhost>
2018-04-02 01:12:13 +0100

    Insert update urls and bump version.

--- mvg.inject.ed2khash.user.js
+++ mvg.inject.ed2khash.user.js
@@ -2,10 +2,13 @@
 // @name Unofficial MVGroup ed2khash JavaScript injector
 // @description Inject ed2khash interface over Java Applet.
 // @namespace mvg.inject.ed2khash
-// @version 1
+// @version 2
 // @match *://docuwiki.net/postbot/
 // @match *://forums.mvgroup.org/releasedb/
 // @grant GM_getResourceText
+// @homepageURL https://github.com/w9d/mvginject
+// @updateURL https://raw.githubusercontent.com/w9d/mvginject/master/mvg.inject.ed2khash.meta.js
+// @downloadURL https://raw.githubusercontent.com/w9d/mvginject/master/mvg.inject.ed2khash.user.js
 // @resource worker https://raw.githubusercontent.com/w9d/ed2khash/833e80c484833982517b4c7b1f0e77ceeb677e96/build/md4-worker.min.js#sha256=d0330e838f487c4338d1b114f78e58045c36cbac0d1bfff6fa601e35be44eaee
 // @require https://raw.githubusercontent.com/w9d/ed2khash/833e80c484833982517b4c7b1f0e77ceeb677e96/build/ed2khash.min.js#sha256=6e1230f73f04112d50a4082744a84ccc725e89ad8ea7c93673461852180ca353
 // @require https://raw.githubusercontent.com/emn178/js-sha256/189bb9b03782b80e59516dfbea78f16b5d9754ce/build/sha256.min.js#sha256=7157511697db744d384a5a2a8646af23f3c90560abf93bb240fdd690b29a898a
