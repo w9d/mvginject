7ae3fd99026d6bca4edc1768cf59ef4ab7787f4a
w9d <w9d@localhost>
2018-05-16 19:54:39 +0100

    mvginject 7
    Changes:
    
    - Compile fixes for long.js and md4.js (using goog.module and goog.require
      instead of mix of ES6 module support which caused compatibility problems)
    
    - md4.js has been trimmed and the JavaScript closure removed
    
    - Files that were less than 65536 bytes would throw RangeError if not
      multiple of 8. These errors are now silently ignored, just like the
      Java Applet

--- mvg.inject.ed2khash.user.js
+++ mvg.inject.ed2khash.user.js
@@ -2,15 +2,15 @@
 // @name Unofficial MVGroup HTML5 eD2k hasher injector
 // @description Inject ed2khash interface over Java Applet.
 // @namespace mvg.inject.ed2khash
-// @version 6
+// @version 7
 // @match *://docuwiki.net/postbot/
 // @match *://forums.mvgroup.org/releasedb/
 // @grant GM_getResourceText
 // @homepageURL https://github.com/w9d/mvginject
 // @updateURL https://raw.githubusercontent.com/w9d/mvginject/master/mvg.inject.ed2khash.meta.js
 // @downloadURL https://raw.githubusercontent.com/w9d/mvginject/master/mvg.inject.ed2khash.user.js
-// @resource worker https://raw.githubusercontent.com/w9d/ed2khash/833e80c484833982517b4c7b1f0e77ceeb677e96/build/md4-worker.min.js#sha256=d0330e838f487c4338d1b114f78e58045c36cbac0d1bfff6fa601e35be44eaee
-// @require https://raw.githubusercontent.com/w9d/ed2khash/833e80c484833982517b4c7b1f0e77ceeb677e96/build/ed2khash.min.js#sha256=6e1230f73f04112d50a4082744a84ccc725e89ad8ea7c93673461852180ca353
+// @resource worker https://raw.githubusercontent.com/w9d/ed2khash/48d26e447c5cfbcb9c13863201653ef27b57f6a3/build/md4-worker.min.js#sha256=0b50b862f531dd7e65835e01bd1a54b8a8671d0fb83092b3dcbc8ae92389a657
+// @require https://raw.githubusercontent.com/w9d/ed2khash/48d26e447c5cfbcb9c13863201653ef27b57f6a3/build/ed2khash.min.js#sha256=8391e52030df2aad325fa6c67b779154f38b3ab3f1217c6fdd77cbbccb001af5
 // @require https://raw.githubusercontent.com/w9d/FileSaver.js/5ed507ef8aa53d8ecfea96d96bc7214cd2476fd2/FileSaver.min.js#sha256=14f249b7c9c0fb12f8454ebf82cae203ca7cc4078b19ab68c938e576f40a19d1
 // ==/UserScript==
 
