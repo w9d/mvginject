bc049887a8afaf698d2f77eab129f909461c3d06
w9d <w9d@localhost>
2020-11-27 03:47:35 +0000

    mvginject 20201126.1 beta
     - allow ed2khash to be reused when torrent parsing fails

--- mvg.inject.ed2khash.user.js
+++ mvg.inject.ed2khash.user.js
@@ -2,7 +2,7 @@
 // @name Unofficial MVGroup HTML5 eD2k hasher injector
 // @description Inject ed2khash interface over Java Applet.
 // @namespace mvg.inject.ed2khash
-// @version 20201126.0
+// @version 20201126.1
 // @match *://docuwiki.net/postbot/
 // @match *://docuwiki.net/postbotbeta/
 // @match *://forums.mvgroup.org/releasedb/
@@ -10,8 +10,8 @@
 // @homepageURL https://github.com/w9d/mvginject
 // @updateURL https://raw.githubusercontent.com/w9d/mvginject/beta/mvg.inject.ed2khash.meta.js
 // @downloadURL https://raw.githubusercontent.com/w9d/mvginject/beta/mvg.inject.ed2khash.user.js
-// @resource worker https://raw.githubusercontent.com/w9d/ed2khash/c006035d2ad4c5742a09f115ad0c483114970251/build/md4-worker.min.js#sha256=19995c6e7e9a231e96326bf09db243784835d497f7bc25c313b50197dcbe6af6
-// @require https://raw.githubusercontent.com/w9d/ed2khash/c006035d2ad4c5742a09f115ad0c483114970251/build/ed2khash.min.js#sha256=f6b7bc82d8088e58de8e3eaf262d23ff3b9d88c67bbd98629cd37ac15696be84
+// @resource worker https://raw.githubusercontent.com/w9d/ed2khash/a353a2d2de498b7f8413a84452969f0c4b070ca1/build/md4-worker.min.js#sha256=19995c6e7e9a231e96326bf09db243784835d497f7bc25c313b50197dcbe6af6
+// @require https://raw.githubusercontent.com/w9d/ed2khash/a353a2d2de498b7f8413a84452969f0c4b070ca1/build/ed2khash.min.js#sha256=5db7ccaeee42dffc4b22ed26c45de8bae286f7be15a30c932f9a5bf13b089844
 // @require https://raw.githubusercontent.com/w9d/FileSaver.js/5ed507ef8aa53d8ecfea96d96bc7214cd2476fd2/FileSaver.min.js#sha256=14f249b7c9c0fb12f8454ebf82cae203ca7cc4078b19ab68c938e576f40a19d1
 // ==/UserScript==
 
